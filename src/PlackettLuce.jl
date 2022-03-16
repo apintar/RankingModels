328c0cf788e04ed8b26e8cf1a265c69a0c197da7
Adam L. Pintar <adampintar@gmail.com>
2022-03-16 14:45:48 -0400

    parameterized PlacketteLuce type

--- src/PlackettLuce.jl
+++ src/PlackettLuce.jl
@@ -1,33 +1,8 @@
-# This is the code for a parameterized PlackettLuce type (Distribution). 
-# I'll keep it here for now in case I decide I need it, but the below
-# constructors for the Vector{Float64} and Vector{Int64} cases for p should work
-# fine.
-
-# struct PlackettLuce{T<:Real, TV<:AbstractVector{T}} <: DiscreteMultivariateDistribution
-#     K::Int
-#     n::Int
-#     p::TV
-#     function PlackettLuce(K::Int, n::Int, p::TV) where {T<:Real, TV<:AbstractVector{T}}
-#         check1 = (n > 0)
-#         check2 = (K >= n)
-#         check3 = isprobvec(p)
-#         check4 = length(p) == K
-#         check1 && check2 && check3 && check4 ? new{T, TV}(K, n, p) : error("invalid PlackettLuce parameters")
-#     end
-# end
-# 
-# function PlackettLuce(n::Int, p::TV) where {T<:Real, TV<:AbstractVector{T}}
-#     check1 = (n > 0)
-#     check2 = isprobvec(p)
-#     check3 = length(p) >= n
-#     check1 && check2 && check3 ? PlackettLuce(length(p), n, p) : error("invalid PlackettLuce parameters")
-# end
-
-struct PlackettLuce <: DiscreteMultivariateDistribution
-    K::Int
-    n::Int
-    p::Vector{Float64}
-    function PlackettLuce(K::Int, n::Int, p::Vector{Float64})
+struct PlackettLuce{S<:Integer, T<:Real} <: DiscreteMultivariateDistribution
+    K::S
+    n::S
+    p::Vector{T}
+    function PlackettLuce{S, T}(K::S, n::S, p::Vector{T}) where {S<:Integer, T<:Real}
         check1 = (n > 0)
         check2 = (K >= n)
         check3 = isprobvec(p)
@@ -36,9 +11,12 @@ struct PlackettLuce <: DiscreteMultivariateDistribution
     end
 end
 
-function PlackettLuce(K::Int, n::Int, p::Vector{Int64})
-    pf = convert(Vector{Float64}, p)
-    PlackettLuce(K, n, pf)
+function PlackettLuce(K::S, n::S, p::Vector{T}) where {S<:Integer, T<:Real}
+    PlackettLuce{S, T}(K, n, p)
 end
 
-test() = println("test3")
+function PlackettLuce(n::S, p::Vector{T}) where {S<:Integer, T<:Real}
+    PlackettLuce(promote(length(p), n), p)
+end
+
+test(a::T) where {T<:Real} = println(a+1)
--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,23 +1,23 @@
-# This is the code for a parameterized PlackettLuce type (Distribution).  I'll
-# keep it here for now in case I decide I need it, but the constructors for the
-# Vector{Float64} and Vector{Int64} cases for p should work fine.
+#= 
+These are non-parameterized versions of the PlackettLuce composite type
+(distribution).  Would need to other outside constructors for convenience for
+types of p other than Float64.  Changed to template instead.
+=#
 
-struct PlackettLuce{T<:Real, TV<:AbstractVector{T}} <: DiscreteMultivariateDistribution
+struct PlackettLuce <: DiscreteMultivariateDistribution
     K::Int
     n::Int
-    p::TV
-    function PlackettLuce(K::Int, n::Int, p::TV) where {T<:Real, TV<:AbstractVector{T}}
+    p::Vector{Float64}
+    function PlackettLuce(K::Int, n::Int, p::Vector{Float64})
         check1 = (n > 0)
         check2 = (K >= n)
         check3 = isprobvec(p)
         check4 = length(p) == K
-        check1 && check2 && check3 && check4 ? new{T, TV}(K, n, p) : error("invalid PlackettLuce parameters")
+        check1 && check2 && check3 && check4 ? new(K, n, p) : error("invalid PlackettLuce parameters")
     end
 end
 
-function PlackettLuce(n::Int, p::TV) where {T<:Real, TV<:AbstractVector{T}}
-    check1 = (n > 0)
-    check2 = isprobvec(p)
-    check3 = length(p) >= n
-    check1 && check2 && check3 ? PlackettLuce(length(p), n, p) : error("invalid PlackettLuce parameters")
+function PlackettLuce(K::Int, n::Int, p::Vector{Int64})
+    pf = convert(Vector{Float64}, p)
+    PlackettLuce(K, n, pf)
 end
