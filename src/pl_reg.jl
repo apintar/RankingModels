f5bef284df0b37e87ac1e057f55ca3d957f939b5
Adam L. Pintar <adampintar@gmail.com>
2022-04-18 22:29:08 -0400

    started regression models

---
+++ src/pl_reg.jl
@@ -0,0 +1,8 @@
+function pl_reg_ll(beta::AbstractVector{T}, 
+                   X::AbstractMatrix{T}, 
+                   o::AbstractMatrix{S}) where {T<:Real, S<:Integer}
+end
+
+function fit_pl_reg(X::AbstractMatrix{T}, 
+                    o::AbstractMatrix{S}) where {T<:Real, S<:Integer}
+end
